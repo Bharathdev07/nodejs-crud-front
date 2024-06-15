62c2f848f0c0e6815cfe13ee45c3ff0937360f65
Bharathdev07 <devops854@gmail.com>
2024-06-15 15:09:21 +0530

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.baseURL || "http://localhost:3000",
+  baseURL: process.env.baseURL || "http://13.232.52.85:3000",
 });
 
 export default api;
